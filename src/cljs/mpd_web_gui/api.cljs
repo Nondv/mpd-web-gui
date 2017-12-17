0c2c90ee7f5aa92591f16ba316115615f2cef32b
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-17 16:07:46 +0300

    refactor api ns by creating function make-request

--- src/cljs/mpd_web_gui/api.cljs
+++ src/cljs/mpd_web_gui/api.cljs
@@ -1,42 +1,31 @@
 (ns mpd-web-gui.api
   (:require [ajax.core :as ajax]))
 
-(defn status [handler]
-  (ajax/POST "http://192.168.0.14:6789"
-             {:response-format (ajax/json-response-format {:keywords? true})
-              :format (ajax/json-request-format)
-              :params  {:action "status"}
-              :handler handler}))
+(defn make-request
+  ([action handler] (make-request action {} handler))
+  ([action params handler]
+   (ajax/POST "http://192.168.0.14:6789"
+              {:response-format (ajax/json-response-format {:keywords? true})
+               :format (ajax/json-request-format)
+               :params  (assoc params :action action)
+               :handler handler})))
 
 (defn log-response [response-map]
   (js/console.log (str response-map)))
 
+(defn status [handler]
+  (make-request "status" handler))
+
 (defn play-next []
-  (ajax/POST "http://192.168.0.14:6789"
-             {:response-format (ajax/json-response-format {:keywords? true})
-              :format (ajax/json-request-format)
-              :params  {:action "next"}
-              :handler log-response}))
+  (make-request "next" log-response))
 
 (defn play-previous []
-  (ajax/POST "http://192.168.0.14:6789"
-             {:response-format (ajax/json-response-format {:keywords? true})
-              :format (ajax/json-request-format)
-              :params  {:action "previous"}
-              :handler log-response}))
+  (make-request "previous" log-response))
 
 (defn pause []
-  (ajax/POST "http://192.168.0.14:6789"
-             {:response-format (ajax/json-response-format {:keywords? true})
-              :format (ajax/json-request-format)
-              :params  {:action "pause"}
-              :handler log-response}))
+  (make-request "pause" log-response))
 
 (defn play
   ([] (play nil))
   ([position]
-   (ajax/POST "http://192.168.0.14:6789"
-              {:response-format (ajax/json-response-format {:keywords? true})
-               :format (ajax/json-request-format)
-               :params  {:action "play" :position position}
-               :handler log-response})))
+   (make-request "play" {:position position} log-response)))
